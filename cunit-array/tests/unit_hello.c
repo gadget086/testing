b3de082ee6e78b6abe1890c919e412333fbdc005
Patrick <irishgadget86@gmail.com>
2020-06-12 10:07:31 -0400

    change build

--- cunit-array/tests/unit_hello.c
+++ cunit-array/tests/unit_hello.c
@@ -5,26 +5,27 @@
 #include <hello/hello.h>
 #include <err.h>
 
-typedef struct {
-	char * name;
-	void (*func)(void);
-} Test;
-
-#define TEST(name) {#name, test_ ## name}
-
 int init_test(void)
 {
   printf("Hello, world from init!\n");
+  return 0;
 }
 
 void test_hello_say(void) {
+  CU_ASSERT(1==2);
+  hello_say();
+}
+
+void test_hello_say2(void) {
   CU_ASSERT_EQUAL(1, 1);
   CU_ASSERT_EQUAL(1, 2);
   hello_say();
 }
+
 int cleanup_test(void)
 {
   printf("Hello, world from cleanup\n");
+  return 0;
 }
 
 void fireball(void) { \
@@ -35,24 +36,27 @@ void fireball(void) { \
 int main(void) 
 { 
 	CU_pSuite       tsuite = NULL; 
-	unsigned int    fails; 
+	//unsigned int    fails; 
 	if (!(CUE_SUCCESS == CU_initialize_registry())) { 
 	  errx(666, "failed to initialise test registry"); 
 	  return EXIT_FAILURE; 
 	}
-	tsuite = CU_add_suite(suite, init_test, cleanup_test); \
+
+	tsuite = CU_add_suite("hello", init_test, cleanup_test); \
 	if (NULL == tsuite) 
-	  fireball(); 
-	do { 
-	  Test arr[] = {TEST(hello_say}; 
-	  for(int i = 0; i < sizeof(arr)/sizeof(Test); i++) { 
-            if(NULL == CU_add_test(tsuite, arr[i].name, arr[i].func)) 
-	      fireball(); 
-	  } 
-	}while(0); 
+	{
+        fireball(); 
+    }
+
+    if((NULL == CU_add_test(tsuite, "test_hello_say",test_hello_say)) ||
+       (NULL == CU_add_test(tsuite, "test_hello_say2",test_hello_say2))) 
+    {
+        fireball(); 
+    }
+	//CU_basic_set_mode(CU_BRM_VERBOSE); 
 	CU_basic_run_tests(); 
-	fails = CU_get_number_of_tests_failed(); 
-	warnx("%u tests failed", fails); 
+	//fails = CU_get_number_of_tests_failed(); 
+	//warnx("%u tests failed", fails); 
 	CU_cleanup_registry(); 
-	return fails; 
+	return CU_get_error(); 
 }
