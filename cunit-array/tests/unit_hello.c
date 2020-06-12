8b31009c5f6126464e4e7c6bd1e43d00ce6ab5c7
Patrick <irishgadget86@gmail.com>
2020-06-12 09:18:22 -0400

    edit unit tests

--- cunit-array/tests/unit_hello.c
+++ cunit-array/tests/unit_hello.c
@@ -1,23 +1,58 @@
+#include <sys/types.h>
+#include <CUnit/CUnit.h>
+#include <CUnit/Basic.h>
+#include <stdlib.h>
 #include <hello/hello.h>
+#include <err.h>
 
-#include "cunit.h"
+typedef struct {
+	char * name;
+	void (*func)(void);
+} Test;
 
-INIT(
+#define TEST(name) {#name, test_ ## name}
+
+int init_test(void)
+{
   printf("Hello, world from init!\n");
-)
-
-CHECK(hello_say) {
-  CU_ASSERT_EQUAL(1, 1);
-  hello_say();
 }
 
-CHECK(hello_say1) {
+void test_hello_say(void) {
   CU_ASSERT_EQUAL(1, 1);
+  CU_ASSERT_EQUAL(1, 2);
   hello_say();
 }
-
-CLEANUP(
+int cleanup_test(void)
+{
   printf("Hello, world from cleanup\n");
-)
+}
 
-RUN("hello", TEST(hello_say), TEST(hello_say1));
+void fireball(void) { \
+	CU_cleanup_registry(); \
+	exit(CU_get_error()); \
+}
+
+int main(void) 
+{ 
+	CU_pSuite       tsuite = NULL; 
+	unsigned int    fails; 
+	if (!(CUE_SUCCESS == CU_initialize_registry())) { 
+	  errx(666, "failed to initialise test registry"); 
+	  return EXIT_FAILURE; 
+	}
+	tsuite = CU_add_suite(suite, init_test, cleanup_test); \
+	if (NULL == tsuite) 
+	  fireball(); 
+	do { 
+	  Test arr[] = {TEST(hello_say}; 
+	  for(int i = 0; i < sizeof(arr)/sizeof(Test); i++) { 
+            if(NULL == CU_add_test(tsuite, arr[i].name, arr[i].func)) 
+	      fireball(); 
+	  } 
+	}while(0); 
+	CU_basic_run_tests(); 
+	fails = CU_get_number_of_tests_failed(); 
+	warnx("%u tests failed", fails); 
+	CU_cleanup_registry(); 
+	return fails; 
+}
